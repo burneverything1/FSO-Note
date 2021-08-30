d06d39a6fe2114a391f8b849527d8a2f2953bb05
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-08-30 17:34:23 -0400

    updated error middleware

--- controllers/notes.js
+++ controllers/notes.js
@@ -37,7 +37,7 @@ notesRouter.post('/', async (request, response, next) => {
     // checks for user token
     const token = getTokenFrom(request)
     const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    if (!token || !decodedToken.id) {       // if no token or token doesn't contain user
         return response.status(401).json({ error: 'token missing or invalid' })
     }
 
