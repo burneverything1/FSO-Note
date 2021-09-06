779119fb1c339fd43dffcfe1d27ae4424ea046a8
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-09-06 19:21:42 -0400

    fixing backend url

--- src/util/axios_config.js
+++ src/util/axios_config.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    
+    baseURL: 'https://ancient-bayou-65086.herokuapp.com/'
 })
 
 export default instance
